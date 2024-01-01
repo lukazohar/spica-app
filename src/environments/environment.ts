8d7af9f808e0fc17979a641ca8abd4daf9600539
Luka Žohar <luka.zohi@gmail.com>
2024-01-01 22:57:12 +0100

    Reading environment variables from .env files. Source: https://github.com/chihab/ngx-env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,7 @@
-export const environment = {};
+export const environment = {
+    PRODUCTION: import.meta.env.NG_APP_PRODUCTION,
+    API_URL: import.meta.env.NG_APP_API_URL,
+    TOKEN_ENDPOINT: import.meta.env.NG_APP_TOKEN_ENDPOINT,
+    CLIENT_ID: import.meta.env.NG_APP_CLIENT_ID,
+    CLIENT_SECRET: import.meta.env.NG_APP_CLIENT_SECRET
+};
