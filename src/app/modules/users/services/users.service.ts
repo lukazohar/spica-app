8b1b689c918e6ccb5205dfa6d9ab55cf16fdf906
Luka Žohar <luka.zohi@gmail.com>
2024-01-02 17:56:25 +0100

    Users lazy loading

--- src/app/modules/users/services/users.service.ts
+++ src/app/modules/users/services/users.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IUser } from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUser(id: string): Observable<IUser> {
+    return this.http.get<IUser>(`${environment.API_URL}/users/${id}`);
+  }
+
+  getUsers(): Observable<IUser[]> {
+    return this.http.get<IUser[]>(`${environment.API_URL}/users`);
+  }
+
+  addUser(data: string): Observable<IUser> {
+    return this.http.post<IUser>(`${environment.API_URL}/users`, data);
+  }
+
+  editUser(id: string, data: IUser): Observable<IUser> {
+    return this.http.put<IUser>(`${environment.API_URL}/users/${id}`, data);
+  }
+
+  deleteUsers(id: string): Observable<IUser> {
+    return this.http.delete<IUser>(`${environment.API_URL}/users/${id}`);
+  }
 }
