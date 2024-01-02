c7d8a86f1ca58d1941a6b3a6cc4f830062ce7af9
Luka Žohar <luka.zohi@gmail.com>
2024-01-02 17:00:10 +0100

    Generated empty users module with service, component and interface

---
+++ src/app/modules/users/users.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UsersComponent } from './components/users/users.component';
+
+@NgModule({
+  declarations: [
+    UsersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
